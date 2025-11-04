8ce9826f65418aeeba6f33cd54b0889a4e9c3bf3
eozant <eozant@st.swps.edu.pl>
2025-11-04 20:37:33 +0100

    Add files via upload

---
+++ Travel Wishlist.py
@@ -0,0 +1,18 @@
+places = ["Japan", "Iceland", "New Zealand", "Norway", "Canada"]
+
+print(places)
+
+places.append("Australia")
+
+places.insert(1, "Italy")
+
+places.remove("New Zealand")
+
+places[2] = "South Korea"
+
+places.sort()
+
+places.reverse()
+
+for place in places:
+    print(f"- {place}")
